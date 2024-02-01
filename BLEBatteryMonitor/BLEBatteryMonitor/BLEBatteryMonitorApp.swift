9070a71c8d9501acd9ef765d5a3534eed79b90aa
Luther Stanton <luther.stanton@gmail.com>
2024-02-01 17:59:29 -0500

    initial project commit

---
+++ BLEBatteryMonitor/BLEBatteryMonitor/BLEBatteryMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEBatteryMonitorApp.swift
+//  BLEBatteryMonitor
+//
+//  Created by Luther Stanton on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEBatteryMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
